eddec553e37ece4d3bf915b510cdda6a96062067
AMIN AKHTER <120783238+Aminakhter1@users.noreply.github.com>
2025-05-01 01:41:32 +0530

    Delete backend directory

--- backend/routes/noteRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const router = express.Router();
-const { createNote } = require("../controllers/noteController");
-const protect = require("../middleware/authMiddleware");
-
-router.post("/", protect, createNote);
-
-module.exports = router;
